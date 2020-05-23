140cba00b39cac082f4a08dbac34bc1c73e4aa33
Wojciech Paja <wpaja@9bits.pl>
2020-05-23 23:05:27 +0200

    leczenie

--- creep.repairer.js
+++ creep.repairer.js
@@ -2,6 +2,21 @@ var utils = require('utils.creep');
 
 var roleRepairer = {
     run: function(creep) {
+        if(typeof(creep.body.find((x) => x.type == HEAL)) != 'undefined')
+        {
+            var ill = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+                filter: function(object) {
+                    return object.hits < object.hitsMax;
+                }
+            });
+            if(ill) {
+                if(creep.heal(ill) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(ill);
+                }
+                return;
+            }
+        }
+
         if(creep.memory.work && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.work = false;
 	    }
