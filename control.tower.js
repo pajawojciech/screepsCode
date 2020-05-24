8883d04684731b6524d1efe0c4ea4fd82c0639db
Wojciech Paja <wpaja@9bits.pl>
2020-05-24 17:24:17 +0200

    wiele wież, harvester

--- control.tower.js
+++ control.tower.js
@@ -3,34 +3,37 @@ var roleTower = {
     {
         var towers = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {filter: (x) => x.structureType == STRUCTURE_TOWER });
         if(towers.length > 0) {
-            var tower = towers[0];
-            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if(closestHostile) {
-                tower.attack(closestHostile);
-            }
-            /*else
+            for(var i in towers)
             {
-                var enemySt = tower.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-                console.log(enemySt);
-                tower.attack(enemySt);
-            }*/
-            else if(tower.store.getUsedCapacity(RESOURCE_ENERGY) > 700)
-            {
-                var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL 
-                });
-                if(closestDamagedStructure) {
-                    tower.repair(closestDamagedStructure);
+                var tower = towers[i];
+                var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if(closestHostile) {
+                    tower.attack(closestHostile);
                 }
-                else
+                /*else
                 {
-                    var ill = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
-                        filter: function(object) {
-                            return object.hits < object.hitsMax;
-                        }
+                    var enemySt = tower.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+                    console.log(enemySt);
+                    tower.attack(enemySt);
+                }*/
+                else if(tower.store.getUsedCapacity(RESOURCE_ENERGY) > 700)
+                {
+                    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                        filter: (structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL 
                     });
-                    if(ill) {
-                        tower.heal(ill);
+                    if(closestDamagedStructure) {
+                        tower.repair(closestDamagedStructure);
+                    }
+                    else
+                    {
+                        var ill = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+                            filter: function(object) {
+                                return object.hits < object.hitsMax;
+                            }
+                        });
+                        if(ill) {
+                            tower.heal(ill);
+                        }
                     }
                 }
             }
--- creep.harvester.js
+++ creep.harvester.js
@@ -4,10 +4,11 @@ var roleHarvester = {
     run: function(creep) {
         var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_TOWER) && 
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                return (
+                    (structure.structureType == STRUCTURE_EXTENSION ||
+                    structure.structureType == STRUCTURE_SPAWN ) && 
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0) 
+                    || (structure.structureType == STRUCTURE_TOWER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 150);
             }
         });
         
